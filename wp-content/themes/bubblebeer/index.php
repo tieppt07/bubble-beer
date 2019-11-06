f2e3c8d2e1d84db34a7dc7a5b5d1d09960a74391
tieppt07 <tieppt07@gmail.com>
2019-11-06 21:37:50 +0700

    index

--- wp-content/themes/bubblebeer/index.php
+++ wp-content/themes/bubblebeer/index.php
@@ -1,8 +1,10 @@
 <?php
-/**
- * Template Name: home page
- */
-get_header(); ?>
+    /**
+     * Template Name: home page
+     */
+    get_header();
+    global $post;
+ ?>
 
 <!-- Page Header -->
 <!-- Set your background image for this header on the line below. -->
@@ -24,51 +26,18 @@ get_header(); ?>
 <div class="container">
 	<div class="row">
 		<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-			<div class="post-preview">
-				<a href="post.html">
-					<h2 class="post-title">
-						Man must explore, and this is exploration at its greatest
-					</h2>
-					<h3 class="post-subtitle">
-						Problems look mighty small from 150 miles up
-					</h3>
-				</a>
-				<p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
-			</div>
-			<hr>
-			<div class="post-preview">
-				<a href="post.html">
-					<h2 class="post-title">
-						I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-					</h2>
-				</a>
-				<p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
-			</div>
-			<hr>
-			<div class="post-preview">
-				<a href="post.html">
-					<h2 class="post-title">
-						Science has not yet mastered prophecy
-					</h2>
-					<h3 class="post-subtitle">
-						We predict too much for the next year and yet far too little for the next ten.
-					</h3>
-				</a>
-				<p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</p>
-			</div>
-			<hr>
-			<div class="post-preview">
-				<a href="post.html">
-					<h2 class="post-title">
-						Failure is not an option
-					</h2>
-					<h3 class="post-subtitle">
-						Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-					</h3>
-				</a>
-				<p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on July 8, 2014</p>
-			</div>
-			<hr>
+            <?php
+                $posts = get_posts();
+                if ( $posts ): foreach ($posts as $post): setup_postdata( $post ); ?>
+                <div class="post-preview">
+                    <a href="<?php the_permalink(); ?>">
+                        <h2 class="post-title"><?php the_title(); ?></h2>
+                        <h3 class="post-subtitle"><?php the_excerpt(); ?></h3>
+                    </a>
+                    <p class="post-meta">Posted on <?php the_time(); ?></p>
+                </div>
+                <hr>
+            <?php endforeach; endif; ?>
 			<!-- Pager -->
 			<ul class="pager">
 				<li class="next">
